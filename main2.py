7e9b61f0aac77fe026ec816f337f7d54b631d0b7
vladyslavius <marakhovskyiv@gmail.com>
2023-01-09 15:34:29 +0200

    :art: Correct algorithm of generation

--- main2.py
+++ main2.py
@@ -1,5 +1,6 @@
 import concurrent.futures
 import logging
+import string
 from typing import NamedTuple, Final, Iterator
 from custom_logger import CustomFormatter
 from math import ceil, floor
@@ -29,11 +30,11 @@ def generate_words_for_current_package(alphabet: str, number_of_start_word:int, 
     logger.warning(f'{package_number=}, {number_of_start_word=}, {word_length=}, {start_word_as_digits=}, {word=}')
     list_of_package_words = []
     for i in range(qtty_of_items_in_package):
-        word_as_digits = list(convert_decimal_number_to_custom_base(number=number_of_start_word, base=alphabet_length))
+        word_as_digits = list(convert_decimal_number_to_custom_base(number=number_of_start_word, base=alphabet_length, word_length=word_length))
         word = ''.join([alphabet[character_index] for character_index in word_as_digits])
         logger.info(f'{word}')
         number_of_start_word+=1
-        if len(word) <= 5:
+        if word not in list_of_package_words:
             list_of_package_words.append(word)
         else:
             break
@@ -46,14 +47,32 @@ def __wrapper(args: ActionArgs) -> int:
     return generate_words_for_current_package(**args._asdict())
 
 
-def convert_decimal_number_to_custom_base(number: int, base: int) -> Iterator[int]:
+# def convert_decimal_number_to_custom_base(number: int, base: int) -> Iterator[int]:
+#     """Convert decimal integer to list of numbers for custom base by iteration. From lowest to bigger."""
+#     number_to_convert = number
+#     while number_to_convert:
+#         floor_division, remainder = divmod(number_to_convert, base)
+#         yield remainder
+#
+#         number_to_convert = floor_division
+
+def convert_decimal_number_to_custom_base(
+        number: int,
+        base: int,
+        word_length: int) -> Iterator[int]:
     """Convert decimal integer to list of numbers for custom base by iteration. From lowest to bigger."""
+
+    list_of_numbers_to_convert = [0 for _ in range(word_length)]
+    counter = word_length - 1
     number_to_convert = number
+
     while number_to_convert:
         floor_division, remainder = divmod(number_to_convert, base)
-        yield remainder
-
+        list_of_numbers_to_convert[counter] = remainder
         number_to_convert = floor_division
+        counter -= 1
+
+    return list_of_numbers_to_convert
 
 def main_2():
     packages = [
@@ -61,8 +80,8 @@ def main_2():
             alphabet=alphabet,
             number_of_start_word=qtty_of_items_in_package * package_number,
             word_length=word_length,
-            start_word_as_digits=list(convert_decimal_number_to_custom_base(number=qtty_of_items_in_package * package_number, base=alphabet_length)),
-            word=''.join([alphabet[character_index] for character_index in list(convert_decimal_number_to_custom_base(number=qtty_of_items_in_package * package_number, base=alphabet_length))]),
+            start_word_as_digits=list(convert_decimal_number_to_custom_base(number=qtty_of_items_in_package * package_number, base=alphabet_length, word_length=word_length)),
+            word=''.join([alphabet[character_index] for character_index in list(convert_decimal_number_to_custom_base(number=qtty_of_items_in_package * package_number, base=alphabet_length, word_length=word_length))]),
             package_number=package_number
         )
         for package_number in range(qtty_of_packages)
@@ -81,7 +100,7 @@ def main_2():
 if __name__ == "__main__":
     alphabet = 'abc'
     alphabet_length = len(alphabet)
-    word_length = 5
+    word_length = 4
     total_number_of_words: Final[int] = len(alphabet) ** word_length
     logger.error(f'{alphabet=}, {word_length=}, {total_number_of_words=}')
     qtty_of_items_in_package = 10
